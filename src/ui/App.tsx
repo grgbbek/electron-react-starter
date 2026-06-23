02b444ed3ecf1da3ff6085f5782dca38635885f3
bibek.gurung <bibek.gurung@3ptrades.com>
2026-06-23 23:41:52 +0545

    Connecting with UI

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // @ts-ignore
+    window.electron.subscribeStatistics((stats) => console.log(stats));
+  }, []);
 
   return (
     <>
@@ -35,7 +40,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
